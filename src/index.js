a0dc2be6bf9761cfff819878a64985eb420375c8
Ritik kumar <63738571+HrithikArya@users.noreply.github.com>
2021-11-05 23:06:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-// import Form from './Form.js'
+// import App from './App';
+// import Form from './Form.js';
+import JokeGenerator from './JokeGenerator';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Form /> */}
+    <JokeGenerator />
   </React.StrictMode>,
   document.getElementById('root')
 );
