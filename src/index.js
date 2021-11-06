d0dba5950d1b3d3263986e5a2904d37c961f03d3
Ritik kumar <hrithikarya@github.com>
2021-11-06 14:20:29 +0530

    joke generator deployed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import Form from './Form.js'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App';
+// import Form from './Form.js';
+import JokeGenerator from "./JokeGenerator";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Form /> */}
+    <JokeGenerator />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
- 
